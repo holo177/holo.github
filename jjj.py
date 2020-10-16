36dae3e9ee029f888e37ac9300cc1443aae97bfe
holo177 <1795518967@qq.com>
2020-10-16 16:32:05 +0800

    from math import * import matplotlib.pyplot as plt import numpy as np
    #取fs=20的时候
    A=2
    f0=2
    Ts=0.05
    fs=20
    n=np.arange(60)
    yn1=[]
    yn2=[]
    yn3=[]
    yn4=[]
    
    for i in range(60):
        c=A*exp(-0.5*i*Ts)*sin(2*pi*f0*i*Ts)
        yn1.append(c)
    plt.stem(n,yn1)#画火柴图
    plt.title('fs=20',fontsize=50)
    plt.xlabel('n',fontsize=25)
    plt.ylabel('yn1',fontsize=25)
    plt.show()
    
    #取fs等于1Hz
    fs=1
    Ts=1
    for i in range(60):
        c=A*exp(-0.5*i*Ts)*sin(2*pi*f0*i*Ts)
        yn2.append(c)
    plt.stem(n,yn2)#画火柴图
    plt.title('fs=1',fontsize=50)
    plt.xlabel('n',fontsize=25)
    plt.ylabel('yn2',fontsize=25)
    plt.show()
    
    #取fs等于50hz,Ts对应的0.2
    fs=50
    Ts=0.02
    for i in range(60):
        c=A*exp(-0.5*i*Ts)*sin(2*pi*f0*i*Ts)
        yn3.append(c)
    plt.stem(n,yn3)#画火柴图
    plt.title('fs=50',fontsize=50)
    plt.xlabel('n',fontsize=25)
    plt.ylabel('yn3',fontsize=25)
    plt.show()
    
    #取fs等于100hz，对应的Ts等于0.1
    fs=100
    Ts=0.01
    for i in range(60):
        c=A*exp(-0.5*i*Ts)*sin(2*pi*f0*i*Ts)
        yn4.append(c)
    plt.stem(n,yn4)#画火柴图
    plt.title('fs=100',fontsize=50)
    plt.xlabel('n',fontsize=25)
    plt.ylabel('yn4',fontsize=25)
    plt.show()
    from math import *
    import matplotlib.pyplot as plt
    import numpy as np
    
    #取fs=20的时候
    A=2
    f0=2
    Ts=0.05
    fs=20
    n=np.arange(60)
    yn1=[]
    yn2=[]
    yn3=[]
    yn4=[]
    
    for i in range(60):
        c=A*exp(-0.5*i*Ts)*sin(2*pi*f0*i*Ts)
        yn1.append(c)
    plt.stem(n,yn1)#画火柴图
    plt.title('fs=20',fontsize=50)
    plt.xlabel('n',fontsize=25)
    plt.ylabel('yn1',fontsize=25)
    plt.show()
    
    #取fs等于1Hz
    fs=1
    Ts=1
    for i in range(60):
        c=A*exp(-0.5*i*Ts)*sin(2*pi*f0*i*Ts)
        yn2.append(c)
    plt.stem(n,yn2)#画火柴图
    plt.title('fs=1',fontsize=50)
    plt.xlabel('n',fontsize=25)
    plt.ylabel('yn2',fontsize=25)
    plt.show()
    
    #取fs等于50hz,Ts对应的0.2
    fs=50
    Ts=0.02
    for i in range(60):
        c=A*exp(-0.5*i*Ts)*sin(2*pi*f0*i*Ts)
        yn3.append(c)
    plt.stem(n,yn3)#画火柴图
    plt.title('fs=50',fontsize=50)
    plt.xlabel('n',fontsize=25)
    plt.ylabel('yn3',fontsize=25)
    plt.show()
    
    #取fs等于100hz，对应的Ts等于0.1
    fs=100
    Ts=0.01
    for i in range(60):
        c=A*exp(-0.5*i*Ts)*sin(2*pi*f0*i*Ts)
        yn4.append(c)
    plt.stem(n,yn4)#画火柴图
    plt.title('fs=100',fontsize=50)
    plt.xlabel('n',fontsize=25)
    plt.ylabel('yn4',fontsize=25)
    plt.show()

--- jjj.py
+++ jjj.py
@@ -1,38 +1,11 @@
-import math 
-import numpy as np 
-import matplotlib.pyplot as plt 
-import mpl_toolkits.axisartist as axisartist #导入坐标轴加工模块
-plt.rcParams['font.sans-serif']=['SimHei']
-plt.rcParams['axes.unicode_minus']=False
+import numpy as np
+import matplotlib.pyplot as plt
 
-fig=plt.figure(figsize=(6,4)) #新建画布
-ax=axisartist.Subplot(fig,111) #使用axisartist.Subplot方法创建一个绘图区对象ax
-fig.add_axes(ax) #将绘图区对象添加到画布中
-
-def exponential_func(x, a=1/2): #定义指数函数
-  y=math.pow(a, x)
-  return y
-
-X=np.linspace(-4, 4, 40) #构造自变量组
-Y=[exponential_func(x) for x in X] #求函数值
-ax.plot(X, Y) #绘制指数函数
-plt.show()
-# 从此处开始打扮
-print(max(X), max(Y)) #测试一下自变量最大值和因变量最大值，为后面的坐标轴设置依据
-ax.axis[:].set_visible(False) #隐藏原来的实线矩形
-ax.axis["x"]=ax.new_floating_axis(0, 0, axis_direction="bottom") #添加x轴
-ax.axis["y"]=ax.new_floating_axis(1, 0, axis_direction="bottom") #添加y轴
-
-ax.axis["x"].set_axisline_style("-|>", size=1.0) #给x坐标轴加箭头
-ax.axis["y"].set_axisline_style("-|>", size=1.0) #给y坐标轴加箭头
-
-ax.annotate(s='x', xy=(max(X), 0), xytext=(max(X)+0.5, 0.5)) #标注x轴
-ax.annotate(s='y', xy=(0, 1.0), xytext=(-0.5, max(Y)+0.5)) #标注y轴
-
-plt.xlim(-4, 5) #设置横坐标范围
-plt.ylim(-1, 17) #设置纵坐标范围
-X_lim=np.arange(min(X), max(X)+1, 1)
-ax.set_xticks(X_lim) #设置x轴刻度
-Y_lim=np.arange(0, max(Y)+1, 1)
-ax.set_yticks(Y_lim) #设置y轴刻度
-ax.annotate(s=r'$y=a^x$',xy=(3, 10), xytext=(3, 10))
+x = np.linspace(0, 6, 100)  # 把 0 - 6   100等分
+y = np.cos(2 * np.pi * x) * np.exp(-x) + 0.8  #  构造函数
+plt.plot(x, y ,label='cos(2*Pi*x)*(1/e)^x + 0.8',color='red', linewidth=3)
+plt.title('plot图')  # 标题
+plt.xlabel('x')   # x轴标题
+plt.ylabel('y')  # y轴标题
+plt.legend()  #  设置图注
+plt.show()     # 显示这个图标
